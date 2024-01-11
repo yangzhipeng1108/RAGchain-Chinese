316fe98dcdf60f277250a21da547f4a19b0b6305
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2024-01-11 08:45:54 +0800

    Update colbertv2.py

--- retrieval/colbertv2.py
+++ retrieval/colbertv2.py
@@ -1,102 +1,63 @@
+from typing import List, Union
+from uuid import UUID
+
+from langchain.schema import Document
+from langchain.schema.vectorstore import VectorStore
+
+from RAGchain.retrieval.base import BaseRetrieval
+from RAGchain.schema import Passage
+from RAGchain.utils.vectorstore.base import SlimVectorStore
 import requests
 from typing import List, Dict
 from ragatouille import RAGPretrainedModel
 
-from langchain_core.callbacks import CallbackManagerForRetrieverRun
-from langchain_core.documents import Document
-from langchain_core.retrievers import BaseRetriever
-
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-
-class CustomRetriever(BaseRetriever):
-    rag: RAGPretrainedModel
-    k: int = 3
-
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        results = self.rag.search(query=query, k=self.k)
-        return [Document(page_content=doc["content"]) for doc in results]
-
-def get_wikipedia_page(title: str):
+class ColbertV2Retrieval(BaseRetrieval):
     """
-    Retrieve the full text content of a Wikipedia page.
-
-    :param title: str - Title of the Wikipedia page.
-    :return: str - Full text content of the page as raw string.
+    VectorDBRetrieval is a retrieval class that uses VectorDB as a backend.
+    First, embed the passage content using an embedding model.
+    Then, store the embedded vector in VectorDB.
+    When retrieving, embed the query and search the most similar vectors in VectorDB.
+    Lastly, return the passages that have the most similar vectors.
     """
-    # Wikipedia API endpoint
-    URL = "https://en.wikipedia.org/w/api.php"
 
-    # Parameters for the API request
-    params = {
-        "action": "query",
-        "format": "json",
-        "titles": title,
-        "prop": "extracts",
-        "explaintext": True,
-    }
+    def __init__(self, model_path="colbert-ir/colbertv2.0"):
+        """
+        :param vectordb: VectorStore instance. You can all langchain VectorStore classes, also you can use SlimVectorStore for better storage efficiency.
+        """
+        super().__init__()
+        self.RAG = RAGPretrainedModel.from_pretrained(model_path)
 
-    # Custom User-Agent header to comply with Wikipedia's best practices
-    headers = {"User-Agent": "RAGatouille_tutorial/0.0.1 (ben@clavie.eu)"}
+    def ingest(self, passages: List[Passage]):
+        self.RAG.index(
+            collection=[passages],
+            index_name="Miyazaki",
+            max_document_length=180,
+            split_documents=True,
+        )
 
-    response = requests.get(URL, params=params, headers=headers)
-    data = response.json()
+    def retrieve(self, query: str, top_k: int = 5) -> List[Passage]:
+        ids = self.retrieve_id(query, top_k)
+        passage_list = self.fetch_data(ids)
+        return passage_list
 
-    # Extracting page content
-    page = next(iter(data["query"]["pages"].values()))
-    return page["extract"] if "extract" in page else None
+    def retrieve_id(self, query: str, top_k: int = 5) -> List[Union[str, UUID]]:
+        docs = self.rag.search(query=query, k=top_k)
+        return [self.__str_to_uuid(doc.metadata.get('passage_id')) for doc in docs]
 
-def get_rag() -> RAGPretrainedModel:
-    RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
-    return RAG
+    def retrieve_id_with_scores(self, query: str, top_k: int = 5) -> tuple[
+        List[Union[str, UUID]], List[float]]:
+        results = self.vectordb.similarity_search_with_score(query=query, k=top_k)
+        results = results[::-1]
+        docs = [result[0] for result in results]
+        scores = [result[1] for result in results]
+        return [self.__str_to_uuid(doc.metadata.get('passage_id')) for doc in docs], scores
 
-def rag_index(RAG:RAGPretrainedModel) -> RAGPretrainedModel:
-    full_document = get_wikipedia_page("Hayao_Miyazaki")
-    RAG.index(
-        collection=[full_document],
-        index_name="Miyazaki",
-        max_document_length=180,
-        split_documents=True,
-    )
-    return RAG
+    def delete(self, ids: List[Union[str, UUID]]):
+        self.vectordb.delete([str(_id) for _id in ids])
 
-def search(RAG:RAGPretrainedModel, qyery:str = "") ->List[Dict]:
-    results = RAG.search(query="What animation studio did Miyazaki found?", k=3)
-
-def get_retriever(rag:RAGPretrainedModel) -> CustomRetriever:
-    return CustomRetriever(rag=rag)
-
-def get_chain(rag:RAGPretrainedModel):
-    prompt = ChatPromptTemplate.from_template(
-        """Answer the following question based only on the provided context:
-
-    <context>
-    {context}
-    </context>
-
-    Question: {input}"""
-    )
-
-    llm = ChatOpenAI()
-
-    document_chain = create_stuff_documents_chain(llm, prompt)
-    retrieval_chain = create_retrieval_chain(rag.as_langchain_retriever(k=3), document_chain)
-    return retrieval_chain
-
-
-def stream_ask(retrieval_chain, input:str="What animation studio did Miyazaki found?"):
-    for s in retrieval_chain.stream({"input": input}):
-        print(s.get("answer", ""), end="")
-
-def load_rag(index_path:str=".ragatouille/colbert/indexes/Miyazaki/") -> RAGPretrainedModel:
-    return RAGPretrainedModel.from_index(index_path)
-
-
-rag = get_rag()
-rag_index(rag)
-rag = load_rag()
-rag.serach('')
+    @staticmethod
+    def __str_to_uuid(input_str: str) -> Union[str, UUID]:
+        try:
+            return UUID(input_str)
+        except:
+            return input_str
